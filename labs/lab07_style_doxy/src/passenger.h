8109c12ded18f1fc34a698e9ffed8a97a6c4ad36
Daniel Shervheim <danielshervheim@gmail.com>
2020-02-12 13:35:23 -0600

    lab 07: initial commit.

---
+++ labs/lab07_style_doxy/src/passenger.h
@@ -0,0 +1,39 @@
+
+/**
+ * @file passenger.h
+ *
+ * @copyright 2019 3081 Staff, All rights reserved.
+ */
+
+#ifndef PASSENGER_H_
+#define PASSENGER_H_
+
+#include <iostream>
+#include <string>
+
+class Passenger {  // : public Reporter {
+ public:
+  /**
+  * @brief Constructs a passenger with a standard name and id.
+  *
+  *
+  * @param[in] int holding an id number
+  * @param[in] string holding a name of a created passenger.
+  *
+  */
+  explicit Passenger(int = -1, std::string = "Nobody");
+  void Update();
+  void GetOnBus();
+  int GetTotalWait() const;
+  bool IsOnBus() const;
+  int GetDestination() const;
+  void Report() const;
+ private:
+  std::string name_;
+  int destination_stop_id_;
+  int wait_at_stop_;
+  int time_on_bus_;
+  int id_;
+  static int count_;   // global count, used to set ID for new instances
+};
+#endif  // PASSENGER_H_
