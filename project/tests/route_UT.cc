4a5710d78abbfc3224f76d3c54e8e1a5ddaab871
Zixuan Zhang <zhan7230@umn.edu>
2020-03-06 16:22:40 -0600

    Complete unittest for route

--- project/tests/route_UT.cc
+++ project/tests/route_UT.cc
@@ -186,6 +186,133 @@ TEST_F(RouteTests, Constructor) {
   EXPECT_EQ(outputSize1, 0);
   EXPECT_EQ(outputSize2, 0);
 
-  // test 
+  // test GetTotalRouteDistance()
+  int totalSize1 = 0;
+  for (int j = 0; j < 5; j++) {
+    totalSize1 += CC_EB_distances[j];
+  }
+  int totalSize2 = 0;
+  for (int j = 0; j < 5; j++) {
+    totalSize2 += CC_WB_distances[j];
+  }
+  EXPECT_EQ(CC_EB.GetTotalRouteDistance(), totalSize1);
+  EXPECT_EQ(CC_WB.GetTotalRouteDistance(), totalSize2);
 
+  // test GetNextStopDistance()
+  EXPECT_EQ(CC_EB.GetNextStopDistance(), 0);
+  EXPECT_EQ(CC_WB.GetNextStopDistance(), 0);
 };
+
+
+TEST_F(RouteTests, IsAtEnd) {
+  //Stop ** all_stops = new Stop *[12];
+  Stop ** CC_EB_stops = new Stop *[6];
+  Stop ** CC_WB_stops = new Stop *[6];
+  std::list<Stop *> CC_EB_stops_list;  
+  std::list<Stop *> CC_WB_stops_list;
+  
+  //Eastbound stops
+  Stop stop_CC_EB_1(0, 43, -92.5); //West bank station
+  Stop stop_CC_EB_2(1); //student union station
+  Stop stop_CC_EB_3(2, 44.973820, -93.227117); //Oak St & Washington Ave
+  Stop stop_CC_EB_4(3, 45, -94); //before transit
+  Stop stop_CC_EB_5(4, 46, -95); //st paul 1
+  Stop stop_CC_EB_6(5, 47, -96); //st paul 2
+  
+  //Westbound stops
+  Stop stop_CC_WB_1(6, 47, -96); //st paul 2
+  Stop stop_CC_WB_2(7, 46, -95); //st paul 1
+  Stop stop_CC_WB_3(8, 45, -94); //before transit
+  Stop stop_CC_WB_4(9, 44.973820, -93.227117); //Oak St & Washington Ave
+  Stop stop_CC_WB_5(10); //student union station
+  Stop stop_CC_WB_6(11, 43, -92.5); //West bank station
+  
+  CC_EB_stops_list.push_back(&stop_CC_EB_1);
+  CC_EB_stops[0] = &stop_CC_EB_1;
+  CC_EB_stops_list.push_back(&stop_CC_EB_2);
+  CC_EB_stops[1] = &stop_CC_EB_2;
+  CC_EB_stops_list.push_back(&stop_CC_EB_3);
+  CC_EB_stops[2] = &stop_CC_EB_3;
+  CC_EB_stops_list.push_back(&stop_CC_EB_4);
+  CC_EB_stops[3] = &stop_CC_EB_4;
+  CC_EB_stops_list.push_back(&stop_CC_EB_5);
+  CC_EB_stops[4] = &stop_CC_EB_5;
+  CC_EB_stops_list.push_back(&stop_CC_EB_6);
+  CC_EB_stops[5] = &stop_CC_EB_6;
+  CC_WB_stops_list.push_back(&stop_CC_WB_1);
+  CC_WB_stops[0] = &stop_CC_WB_1;
+  CC_WB_stops_list.push_back(&stop_CC_WB_2);
+  CC_WB_stops[1] = &stop_CC_WB_2;
+  CC_WB_stops_list.push_back(&stop_CC_WB_3);
+  CC_WB_stops[2] = &stop_CC_WB_3;
+  CC_WB_stops_list.push_back(&stop_CC_WB_4);
+  CC_WB_stops[3] = &stop_CC_WB_4;
+  CC_WB_stops_list.push_back(&stop_CC_WB_5);
+  CC_WB_stops[4] = &stop_CC_WB_5;
+  CC_WB_stops_list.push_back(&stop_CC_WB_6);
+  CC_WB_stops[5] = &stop_CC_WB_6;
+  
+  double * CC_EB_distances = new double[5];
+  double * CC_WB_distances = new double[5];
+  CC_EB_distances[0] = 5;
+  CC_EB_distances[1] = 4;
+  CC_EB_distances[2] = 3;
+  CC_EB_distances[3] = 10;
+  CC_EB_distances[4] = 6;
+  
+  CC_WB_distances[0] = 6;
+  CC_WB_distances[1] = 10;
+  CC_WB_distances[2] = 3;
+  CC_WB_distances[3] = 4;
+  CC_WB_distances[4] = 5;
+  
+  
+  std::list<double> CC_EB_probs;  //realistic .15, .3, .025, .05, .05, 0
+  CC_EB_probs.push_back(.15); //WB
+  CC_EB_probs.push_back(.3); //CMU
+  CC_EB_probs.push_back(.025); //O&W
+  CC_EB_probs.push_back(.05); //Pre-transit
+  CC_EB_probs.push_back(.05); //STP 1
+  CC_EB_probs.push_back(0); //STP 1
+  //TODO: is this always true? If so, we may want to reduce the length of probs to_char_type
+  //        remove possibility of generating passengers with nowhere to go
+  
+  std::list<double> CC_WB_probs; //realistic .35, .05, .01, .01, .2, 0
+  CC_WB_probs.push_back(.35); //stp 2
+  CC_WB_probs.push_back(.05); //stp 1
+  CC_WB_probs.push_back(.01); //post-transit
+  CC_WB_probs.push_back(.01); //O&W
+  CC_WB_probs.push_back(.02); //CMU
+  CC_WB_probs.push_back(0); //WB
+  
+  
+  RandomPassengerGenerator CC_EB_generator(CC_EB_probs, CC_EB_stops_list);
+  RandomPassengerGenerator CC_WB_generator(CC_WB_probs, CC_WB_stops_list);
+  
+  Route CC_EB("Campus Connector - Eastbound", CC_EB_stops, CC_EB_distances, 6, &CC_EB_generator);
+  Route CC_WB("Campus Connector - Westbound", CC_WB_stops, CC_WB_distances, 6, &CC_WB_generator);
+
+  EXPECT_EQ(CC_EB.IsAtEnd(), false);
+
+  CC_EB.NextStop();
+  CC_EB.NextStop();
+  CC_EB.NextStop();
+  CC_EB.NextStop();
+  CC_EB.NextStop();
+  CC_EB.NextStop();
+
+  EXPECT_EQ(CC_EB.IsAtEnd(), true);
+
+  EXPECT_EQ(CC_WB.IsAtEnd(), false);
+
+  CC_WB.NextStop();
+  CC_WB.NextStop();
+  EXPECT_EQ(CC_WB.IsAtEnd(), false);
+  CC_WB.NextStop();
+  CC_WB.NextStop();
+  CC_WB.NextStop();
+  EXPECT_EQ(CC_WB.IsAtEnd(), false);
+  CC_WB.NextStop();
+
+  EXPECT_EQ(CC_WB.IsAtEnd(), true);
+}
